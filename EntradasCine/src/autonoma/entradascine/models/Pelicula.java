1071c87dad90c807e85b3671a61b1599478a17f4
Kallst <santiago.castrom@autonoma.edu.co>
2025-04-02 14:01:38 -0500

    Se creo la clase Pelicula con sus respectivos atributos y métodos getters y setters

---
+++ EntradasCine/src/autonoma/entradascine/models/Pelicula.java
@@ -0,0 +1,14 @@
+package autonoma.entradascine.models;
+
+/**
+ *
+ * @author Santiago Castro Marles
+ */
+
+public class Pelicula {
+    
+    private String nombre;
+    private float costoBase;
+    
+    
+}

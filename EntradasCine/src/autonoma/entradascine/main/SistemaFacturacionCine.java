1fb1a8e8428ccac4c2de5cdd44b8b8030d8889dc
Kallst <santiago.castrom@autonoma.edu.co>
2025-04-02 13:51:28 -0500

    Se eliminó el main por defecto, y se creo el nuevo main para poder trabajarlo con las respectivas capas del modelo de negocio

---
+++ EntradasCine/src/autonoma/entradascine/main/SistemaFacturacionCine.java
@@ -0,0 +1,12 @@
+package autonoma.entradascine.main;
+
+/**
+ *
+ * @author Santiago Castro Marles
+ */
+public class SistemaFacturacionCine {
+    public static void main (String args [])
+    {
+        
+    }
+}
